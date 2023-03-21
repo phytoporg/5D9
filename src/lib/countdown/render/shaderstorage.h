87a6b44d702dd4a764295de02f4dc7d2a924b1b6
Phil <phytopoorg@gmail.com>
2023-03-20 22:10:54 -0700

    Shader storage initial revision

---
+++ src/lib/countdown/render/shaderstorage.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "shader.h"
+#include <string>
+#include <vector>
+#include <cstdint>
+
+namespace countdown { namespace render {
+    class ShaderStorage
+    {
+    public:
+        ~ShaderStorage();
+
+        bool
+        AddShader(
+            const std::string& vertexText,
+            const std::string& fragmentText,
+            const std::string& shaderName
+            );
+
+        ShaderPtr FindShaderByName(const std::string& shaderName) const;
+
+    private:
+        uint32_t CompileVertexShader(const std::string& source);
+        uint32_t CompileFragmentShader(const std::string& source);
+
+        // LinkShaders
+        bool 
+        LinkShaders(
+            uint32_t programHandle,
+            uint32_t vertexShaderHandle,
+            uint32_t fragmentShaderHandle
+            );
+        std::vector<ShaderAttribute> PopulateAttributes(uint32_t programHandle);
+        std::vector<ShaderUniform> PopulateUniforms(uint32_t programHandle);
+
+        std::vector<ShaderPtr> m_storageVector;
+    };
+}}
