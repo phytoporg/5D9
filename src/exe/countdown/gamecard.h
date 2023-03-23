fb015d3db5cb30b22351bb2fff30b951f1c6daad
Phil <phytopoorg@gmail.com>
2023-03-22 22:04:22 -0700

    gamecard initial revision

---
+++ src/exe/countdown/gamecard.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <countdown/render/mesh.h>
+#include <countdown/render/shader.h>
+
+class GameCard
+{
+public:
+    GameCard(
+        countdown::render::ShaderPtr spShader,
+        const glm::vec3& upperLeft,
+        uint32_t width,
+        uint32_t height);
+
+    void Draw(const glm::mat4& projMatrix, const glm::mat4& viewMatrix);
+
+private:
+    GameCard() = delete;
+    GameCard(const GameCard& other) = delete;
+
+    countdown::render::Mesh m_cardMesh;
+
+    // All game cards are fundamentally textured unit quads.
+    // TODO: instanced rendering for game cards
+    static const glm::vec3 s_Vertices[4];
+    static const glm::vec2 s_UVs[4];
+    static const int       s_Indices[6];
+};
