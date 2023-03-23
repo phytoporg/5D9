38bf45b8470bbdb1576ffb455441489eec7c9859
Phil <phytopoorg@gmail.com>
2023-03-22 17:33:41 -0700

    indexbuffer initial revision

---
+++ src/lib/countdown/render/indexbuffer.cpp
@@ -0,0 +1,64 @@
+#include "indexbuffer.h"
+#include "rendercommon.h"
+
+using namespace countdown::render;
+
+IndexBuffer::IndexBuffer() 
+{
+    glGenBuffers(1, &m_handle);
+}
+
+IndexBuffer::IndexBuffer(const IndexBuffer& other)
+{
+    m_handle = other.m_handle;
+    m_count = other.m_count;
+}
+
+IndexBuffer& IndexBuffer::operator=(const IndexBuffer& other)
+{
+    m_handle = other.m_handle;
+    m_count = other.m_count;
+    return *this;
+}
+
+IndexBuffer::~IndexBuffer() {
+    glDeleteBuffers(1, &m_handle);    
+}
+
+void IndexBuffer::Set(const uint32_t* inputArray, uint32_t arrayLength) 
+{
+    m_count = arrayLength;    
+    const size_t intSize = sizeof(uint32_t);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_handle);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, intSize * arrayLength, inputArray, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+}
+
+void IndexBuffer::Set(const int* inputArray, uint32_t arrayLength) 
+{
+    m_count = arrayLength;    
+    const size_t intSize = sizeof(int);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_handle);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, intSize * arrayLength, inputArray, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+}
+
+void IndexBuffer::Set(const std::vector<uint32_t>& data) 
+{
+    Set(data.data(), static_cast<uint32_t>(data.size()));
+}
+
+void IndexBuffer::Set(const std::vector<int>& data) 
+{
+    Set(data.data(), static_cast<uint32_t>(data.size()));
+}
+
+uint32_t IndexBuffer::Count() const 
+{
+    return m_count;    
+}
+
+uint32_t IndexBuffer::GetHandle() const 
+{
+    return m_handle;
+}
---
+++ src/lib/countdown/render/indexbuffer.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <cstdint>
+#include <vector>
+
+namespace countdown { namespace render {
+    class IndexBuffer
+    {
+    public:
+        IndexBuffer();
+        IndexBuffer(const IndexBuffer& other);
+        IndexBuffer& operator=(const IndexBuffer& other);
+
+        ~IndexBuffer();
+
+        void Set(const uint32_t* inputArray, uint32_t arrayLength);
+        void Set(const int* inputArray, uint32_t arrayLength);
+        void Set(const std::vector<uint32_t>& data);
+        void Set(const std::vector<int>& data);
+
+        uint32_t Count() const;
+        uint32_t GetHandle() const;
+
+    private:
+        uint32_t m_count = 0;
+        uint32_t m_handle = 0;
+    };
+}}
